1b22c28f3ebaed3ce790dfc9bbe2ba690cdcd490
Larz Piechocki <larzpiechocki@gmail.com>
2026-08-02 02:32:45 -0500

    Organize Code into Regions and Update Documentation

--- src/AtomSimulator/QuantumSolver.hs
+++ src/AtomSimulator/QuantumSolver.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeOperators #-}
+{-# OPTIONS_GHC -Wmissing-signatures -Wmissing-local-signatures #-}
+{-# LANGUAGE InstanceSigs #-}
 module AtomSimulator.QuantumSolver where
 
 import AtomSimulator.Units
@@ -15,8 +17,10 @@ import GHC.RTS.Flags (ProfFlags(ccsLength))
 import Data.Metrology.Show
 import Data.Function (on)
 import Text.Printf (printf)
+import Numeric.LinearAlgebra (Convert(double))
 
-{- Creates a 3D potential energy grid from a function that says a given potential energy at a point in space -}
+-- ===REGION Creating Potential Energy Grids
+-- | Creates a 3D potential energy grid from a function that says a given potential energy at a point in space
 createPotentialGrid :: (Length -> Length -> Length -> Energy) -> Natural -> Length -> PhysicalGrid Energy
 createPotentialGrid = createGridFromFuncShifted
 
@@ -24,82 +28,7 @@ createPotentialGrid = createGridFromFuncShifted
 harmonicPotential :: SpringConstant -> Length -> Length -> Length -> Energy
 harmonicPotential = harmonicFunc
 
-{- Given a potential energy 3D space, and any particle specific properties (like mass) this will return an energy
-matrix for solving for possible solutions to this system -}
-hamiltonianMatrix :: PhysicalGrid Energy -> [[Energy]]
-hamiltonianMatrix (PhysicalGrid s v) = [ [ f p q | q <- pts ] | p <- pts ]
-  where
-    pts = range ((0,0,0), snd $ bounds v)
-    kineticCoeff = (0.0381 % ElectronVolt) |*| qSq (1 % Nanometer)
-    k :: Energy
-    k = kineticCoeff |/| qSq s
-    f :: Idx3 -> Idx3 -> Energy
-    f p q
-      | p == q             = (6 |*| k) |+| (v ! p)
-      | manhattan p q == 1 = (-1) |*| k
-      | otherwise          = 0 % ElectronVolt
-
-hamiltonianVector :: forall dim lcsu unit. (Energy ~ Qu dim lcsu Double, ValidDLU dim lcsu unit)
-  => unit -> PhysicalGrid Energy -> (Vec -> Vec, unit)
-hamiltonianVector unit (PhysicalGrid s v) = (matvec, unit)
-  where
-    matvec psi = listArray (bounds v)
-      [ (6 |*| k |*| psiP |+| (v ! p |*| psiP) |-| (k |*| (neighborSum psi p % Number))) # unit
-      | p <- range $ bounds v, let psiP = psi ! p % Number ]
-    kineticCoeff = (0.0381 % ElectronVolt) |*| qSq (1 % Nanometer)
-    k :: Energy
-    k = kineticCoeff |/| qSq s
-    neighborSum psi p = sum [ psi ! q | q <- neighborsOf p (snd $ bounds v) ]
-
--- | Spin is an intrinsic form of angular momentum carried by particles. There are 2 orthogonal states of spin for spin 1/2 spin particles
-data Spin = SpinUp | SpinDown deriving (Eq, Show)
--- | The solution for an electron cloud. This contains the eigen-energy of the particle, a probability density, and the spin of the particle if from a multi-particle solver
-newtype WaveSolution = WaveSolution (Energy, PhysicalGrid Density, Maybe Spin) deriving (Eq)
-{- | The wave function of an electron (psi). This contains the eigen-energy of the particle, a vector containning indexes and results of the
-wavefunction at various points in space, and the particles spin -}
-type WaveState = (Energy, Vec, Spin)
-
-instance Show WaveSolution where
-  show (WaveSolution (e, _, spin)) = case spin of
-    Just s  -> show s ++ " " ++ show e
-    Nothing -> show e
-
-showInHartree :: WaveSolution -> String
-showInHartree (WaveSolution (e, _, spin)) = case spin of
-  Just s  -> show s ++ " " ++ printf "%.4f Hartree" (e # Hartree)
-  Nothing -> printf "%.4f Hartree" (e # Hartree)
-
-instance Ord WaveSolution where
-  compare = comparing (\(WaveSolution (e, _, _)) -> e)
-
-waveToProb :: Length -> Double -> Density
-waveToProb s = (|/| (s |*| s |*| s)) . (% Number) . (^2)
-
-{- Given a grid saying the potential energy in the system, and any particle specific properties (like mass) this will
-solve for possible eigen energies the particle can hold and also for probability densities of the particle at that given
-energy (the eigen vectors) -}
-solveAllOrbitals :: PhysicalGrid Energy -> [WaveSolution]
-solveAllOrbitals (PhysicalGrid s v) = sort (map WaveSolution (zip3 energies densityGrids (repeat Nothing)))
-  where
-    (energies, waveFuncs) = solveMatrix ElectronVolt (hamiltonianMatrix (PhysicalGrid s v))
-    waveFuncGrids :: [Grid3D Double]
-    waveFuncGrids = map (listArray (bounds v)) waveFuncs
-    densitiesGrid3D :: [Grid3D Density]
-    densitiesGrid3D = map (fmap $ waveToProb s) waveFuncGrids
-    densityGrids :: [PhysicalGrid Density]
-    densityGrids = map (PhysicalGrid s) densitiesGrid3D
-
-solveLowestOribtal :: PhysicalGrid Energy -> WaveSolution
-solveLowestOribtal grid@(PhysicalGrid s v) = WaveSolution (energy, densityGrid, Nothing)
-  where
-    (energy, waveFunc) = lanczosLowest (hamiltonianVector ElectronVolt grid) (snd (bounds v))
-    waveFuncGrid  :: Grid3D Double
-    waveFuncGrid = listArray (bounds v) (elems waveFunc)
-    densityGrid3D :: Grid3D Density
-    densityGrid3D = fmap (waveToProb s) waveFuncGrid
-    densityGrid :: PhysicalGrid Density
-    densityGrid = PhysicalGrid s densityGrid3D
-
+-- | Given an electron density, this will find the potential energy for another electron to exist nearby
 coulombPotentialFromDensity :: Charge -> Charge -> PhysicalGrid Density -> PhysicalGrid Energy
 coulombPotentialFromDensity q1 q2 (PhysicalGrid spacing grid) = 
   let
@@ -116,7 +45,115 @@ coulombPotentialFromDensity q1 q2 (PhysicalGrid spacing grid) =
     relCoulombConst = q1 |*| q2 |/| (4 |*| pi |*| vacuumPermittivity)
     potentialAt :: Idx3 -> Energy
     potentialAt p = relCoulombConst |*| sumInverseDists p
+-- ===ENDREGION
 
+-- ===REGION Wave Solution Definitions
+-- | The solution for an electron cloud. This contains the eigen-energy of the particle, a probability density, and the spin of the particle if from a multi-particle solver
+newtype WaveSolution = WaveSolution (Energy, PhysicalGrid Density, Maybe Spin) deriving (Eq)
+{- | The wave function of an electron (psi). This contains the eigen-energy of the particle, a vector containning indexes and results of the
+wavefunction at various points in space, and the particles spin -}
+type WaveState = (Energy, Vec, Spin)
+
+instance Show WaveSolution where
+  show :: WaveSolution -> String
+  show (WaveSolution (e, _, spin)) = case spin of
+    Just s  -> show s ++ " " ++ show e
+    Nothing -> show e
+
+-- | PLEASE DELETE ME
+showInHartree :: WaveSolution -> String
+showInHartree (WaveSolution (e, _, spin)) = case spin of
+  Just s  -> show s ++ " " ++ printf "%.4f Hartree" (e # Hartree)
+  Nothing -> printf "%.4f Hartree" (e # Hartree)
+
+instance Ord WaveSolution where
+  compare = comparing (\(WaveSolution (e, _, _)) -> e)
+
+-- | Given a wave function/result, convert it to a probabilty. (ie. psi -> |psi|^2)
+waveToProb :: Length -> Double -> Density
+waveToProb s = (|/| (s |*| s |*| s)) . (% Number) . (^2)
+-- ===ENDREGION
+
+-- ===REGION One State Solution Matrix Solving
+{- Given a potential energy 3D space, and any particle specific properties (like mass) this will return an energy
+matrix for solving for possible solutions to this system -}
+hamiltonianMatrix :: PhysicalGrid Energy -> [[Energy]]
+hamiltonianMatrix (PhysicalGrid s v) = [ [ f p q | q <- pts ] | p <- pts ]
+  where
+    pts = range ((0,0,0), snd $ bounds v)
+    kineticCoeff = (0.0381 % ElectronVolt) |*| qSq (1 % Nanometer)
+    k :: Energy
+    k = kineticCoeff |/| qSq s
+    f :: Idx3 -> Idx3 -> Energy
+    f p q
+      | p == q             = (6 |*| k) |+| (v ! p)
+      | manhattan p q == 1 = (-1) |*| k
+      | otherwise          = 0 % ElectronVolt
+
+{- Given a grid saying the potential energy in the system, and any particle specific properties (like mass) this will
+solve for possible eigen energies the particle can hold and also for probability densities of the particle at that given
+energy (the eigen vectors) -}
+solveAllOrbitals :: PhysicalGrid Energy -> [WaveSolution]
+solveAllOrbitals (PhysicalGrid s v) = sort (map WaveSolution (zip3 energies densityGrids (repeat Nothing)))
+  where
+    (energies, waveFuncs) = solveMatrix ElectronVolt (hamiltonianMatrix (PhysicalGrid s v))
+    waveFuncGrids :: [Grid3D Double]
+    waveFuncGrids = map (listArray (bounds v)) waveFuncs
+    densitiesGrid3D :: [Grid3D Density]
+    densitiesGrid3D = map (fmap $ waveToProb s) waveFuncGrids
+    densityGrids :: [PhysicalGrid Density]
+    densityGrids = map (PhysicalGrid s) densitiesGrid3D
+-- ===ENDREGION
+
+-- ===REGION One State Solution Vector Solving
+-- | Computes the hamiltonian given a potential energy grid. This is done using vector storage.
+hamiltonianVector :: forall dim lcsu unit. (Energy ~ Qu dim lcsu Double, ValidDLU dim lcsu unit)
+  => unit -> PhysicalGrid Energy -> (Vec -> Vec, unit)
+hamiltonianVector unit (PhysicalGrid s v) = (matvec, unit)
+  where
+    matvec :: Vec -> Vec
+    matvec psi = listArray (bounds v)
+      [ (6 |*| k |*| (psi ! p % Number) |+| (v ! p |*| (psi ! p % Number)) |-| (k |*| (neighborSum psi p % Number))) # unit
+      | p <- range $ bounds v ]
+    kineticCoeff = (0.0381 % ElectronVolt) |*| qSq (1 % Nanometer)
+    k :: Energy
+    k = kineticCoeff |/| qSq s
+    neighborSum :: Vec -> Idx3 -> Double
+    neighborSum psi p = sum [ psi ! q | q <- neighborsOf p (snd $ bounds v) ]
+
+-- | Gets the lowest orbital solution in a 1 particle system using lanczos quick matrix calculations
+solveLowestOribtal :: PhysicalGrid Energy -> WaveSolution
+solveLowestOribtal grid@(PhysicalGrid s v) = WaveSolution (energy, densityGrid, Nothing)
+  where
+    (energy, waveFunc) = lanczosLowest (hamiltonianVector ElectronVolt grid) (snd (bounds v))
+    waveFuncGrid  :: Grid3D Double
+    waveFuncGrid = listArray (bounds v) (elems waveFunc)
+    densityGrid3D :: Grid3D Density
+    densityGrid3D = fmap (waveToProb s) waveFuncGrid
+    densityGrid :: PhysicalGrid Density
+    densityGrid = PhysicalGrid s densityGrid3D
+-- ===ENDREGION
+
+-- ===REGION Orthogonality Forcing
+-- | Generates every combination of spin up/spin down unique assignments for a given number of electrons
+candidateSpinAssignments :: Natural -> [[Spin]]
+candidateSpinAssignments = binaryPartitions SpinUp SpinDown
+
+-- | The bare one-electron energy <psi|T+V_ext|psi> — kinetic + external potential only
+oneElectronEnergy :: PhysicalGrid Energy -> Vec -> Energy
+oneElectronEnergy extPotential psi = (dotA psi (h0 psi) / dotA psi psi) % ElectronVolt
+  where
+    h0 :: Vec -> Vec
+    (h0, _) = hamiltonianVector ElectronVolt extPotential
+
+-- | Correct total Hartree-Fock energy: E = 1/2 * sum(h_i + epsilon_i)
+totalEnergyHF :: PhysicalGrid Energy -> [WaveState] -> Energy
+totalEnergyHF extPotential states =
+  (0.5 % Number) |*| foldl1' (|+|)
+    [ h_i |+| epsilon_i | (epsilon_i, psi, _) <- states, let h_i = oneElectronEnergy extPotential psi ]
+-- ===ENDREGION
+
+-- ===REGION Hartree Fock
 {-| Hartree-Fock exchange contribution. Given the orbitals of electrons already placed into the
 system, this is what gets subtracted from the plain Hamiltonian's action on a trial wavefunction. -}
 exchangeTerm :: Length -> [Vec] -> Vec -> Vec
@@ -141,68 +178,84 @@ hamiltonianVectorHF :: forall dim lcsu unit. (Energy ~ Qu dim lcsu Double, Valid
   => unit -> PhysicalGrid Energy -> [Vec] -> (Vec -> Vec, unit)
 hamiltonianVectorHF unit grid previousOrbitals =
   let
+    localMatvec :: Vec -> Vec
     (localMatvec, _) = hamiltonianVector unit grid
+    fockMatvec :: Vec -> Vec
     fockMatvec psi = localMatvec psi `subA` exchangeTerm (spacing grid) previousOrbitals psi
   in (fockMatvec, unit)
 
--- | Generates every combination of spin up/spin down unique assignments for a given number of electrons
-candidateSpinAssignments :: Natural -> [[Spin]]
-candidateSpinAssignments = binaryPartitions SpinUp SpinDown
-
-runSCF :: Natural -> PhysicalGrid Energy -> [Spin] -> [WaveState]
-runSCF maxIterations extPotential@(PhysicalGrid s v) spins = iterateSCF 0 initialStates
+-- | An pass on adjusting and finding new states when solving for a self consitent field of multiple electrons
+scfPass :: [WaveState] -> PhysicalGrid Energy -> [WaveState]
+scfPass states extPotential = map resolve [0 .. length states - 1]
   where
-    bnd = snd (bounds v)
-    zeroDensity = PhysicalGrid s (fmap (const (waveToProb s 0)) v)
-    densityOf psi = PhysicalGrid s (fmap (waveToProb s) (listArray (bounds v) (elems psi)))
-
-    initialStates :: [WaveState]
-    initialStates = greedyPass spins zeroDensity []
-
-    greedyPass [] _ _ = []
-    greedyPass (mySpin : restSpins) cumulativeDensity previousElectrons =
-      let repulsion          = coulombPotentialFromDensity electronCharge electronCharge cumulativeDensity
-          effectivePotential  = addGrids extPotential repulsion
-          sameSpinPrevious    = [ psi | (psi, sp) <- previousElectrons, sp == mySpin ]
-          (energy, waveFunc)  = lanczosLowestExcluding sameSpinPrevious
-                                  (hamiltonianVectorHF ElectronVolt effectivePotential sameSpinPrevious) bnd
-          newCumulativeDensity = addGrids cumulativeDensity (densityOf waveFunc)
+    othersDensity :: Int -> PhysicalGrid Density
+    othersDensity i = case [ mapVecToGrid extPotential psi waveToProb | (j, (_, psi, _)) <- zip [0 ..] states, j /= i ] of
+      [] -> toZeroDensity extPotential
+      densities -> foldl1' addGrids densities
+    sameSpinOthers :: Int -> Spin -> [Vec]
+    sameSpinOthers i mySpin =
+      [ psi | (j, (_, psi, sp)) <- zip [0 ..] states, j /= i, sp == mySpin ]
+    resolve :: Int -> WaveState
+    resolve i =
+      let
+        mySpin :: Spin
+        (_, _, mySpin) = states !! i
+        repulsion :: PhysicalGrid Energy
+        repulsion = coulombPotentialFromDensity electronCharge electronCharge (othersDensity i)
+        effectivePotential :: PhysicalGrid Energy
+        effectivePotential = addGrids extPotential repulsion
+        occupied :: [Vec]
+        occupied = sameSpinOthers i mySpin
+        energy :: Energy
+        waveFunc :: Vec
+        (energy, waveFunc) = lanczosLowestExcluding occupied
+          (hamiltonianVectorHF ElectronVolt effectivePotential occupied) (bndGrid extPotential)
       in (energy, waveFunc, mySpin)
-          : greedyPass restSpins newCumulativeDensity (previousElectrons ++ [(waveFunc, mySpin)])
 
-    scfPass states = map resolve [0 .. length states - 1]
-      where
-        othersDensity i = case [ densityOf psi | (j, (_, psi, _)) <- zip [0 ..] states, j /= i ] of
-          []        -> zeroDensity
-          densities -> foldl1' addGrids densities
+-- | An initial first greedy pass when doing a self-consitent field setup.
+greedyPass :: [Spin] -> PhysicalGrid Density -> [(Vec, Spin)] -> PhysicalGrid Energy -> [WaveState]
+greedyPass [] _ _ _ = []
+greedyPass (mySpin : restSpins) cumulativeDensity previousElectrons extPotential =
+  let 
+    repulsion :: PhysicalGrid Energy
+    repulsion = coulombPotentialFromDensity electronCharge electronCharge cumulativeDensity
+    effectivePotential :: PhysicalGrid Energy
+    effectivePotential = addGrids extPotential repulsion
+    sameSpinPrevious :: [Vec]
+    sameSpinPrevious = [ psi | (psi, sp) <- previousElectrons, sp == mySpin ]
+    energy :: Energy
+    waveFunc :: Vec
+    (energy, waveFunc)  = lanczosLowestExcluding sameSpinPrevious
+      (hamiltonianVectorHF ElectronVolt effectivePotential sameSpinPrevious) (bndGrid extPotential)
+    newCumulativeDensity :: PhysicalGrid Density
+    newCumulativeDensity = addGrids cumulativeDensity (mapVecToGrid extPotential waveFunc waveToProb)
+  in (energy, waveFunc, mySpin) : greedyPass restSpins newCumulativeDensity (previousElectrons ++ [(waveFunc, mySpin)]) extPotential
 
-        sameSpinOthers i mySpin =
-          [ psi | (j, (_, psi, sp)) <- zip [0 ..] states, j /= i, sp == mySpin ]
-
-        resolve i =
-          let (_, _, mySpin)    = states !! i
-              repulsion          = coulombPotentialFromDensity electronCharge electronCharge (othersDensity i)
-              effectivePotential = addGrids extPotential repulsion
-              occupied           = sameSpinOthers i mySpin
-              (energy, waveFunc) = lanczosLowestExcluding occupied
-                                      (hamiltonianVectorHF ElectronVolt effectivePotential occupied) bnd
-          in (energy, waveFunc, mySpin)
-
+-- | Does a self consistent field calculation of repeated electron orbital position checking and potential energy generating on repeat
+runSCF :: Natural -> PhysicalGrid Energy -> [Spin] -> [WaveState]
+runSCF maxIterations extPotential spins = iterateSCF 0 initialStates
+  where
+    initialStates :: [WaveState]
+    initialStates = greedyPass spins (toZeroDensity extPotential) [] extPotential
+    converged :: [WaveState] -> [WaveState] -> Bool
     converged old new = all (< 1.0e-6) (zipWith energyDiff old new)
-      where energyDiff (e1, _, _) (e2, _, _) = abs ((e1 |-| e2) # ElectronVolt)
-
+      where
+        energyDiff :: WaveState -> WaveState -> Double
+        energyDiff (e1, _, _) (e2, _, _) = abs ((e1 |-| e2) # ElectronVolt)
+    iterateSCF :: Natural -> [WaveState] -> [WaveState]
     iterateSCF itersDone states
       | itersDone >= maxIterations = states
       | converged states next      = next
       | otherwise                  = iterateSCF (itersDone + 1) next
-      where next = scfPass states
+      where
+        next :: [WaveState]
+        next = scfPass states extPotential
 
+-- | Finds hartree fock solutions for total energy and electron orbital solutions given a number of electrons, a max iteration, and a starting external potential
 solveInteractingElectronsHF :: Natural -> Natural -> PhysicalGrid Energy -> (Energy, [WaveSolution])
 solveInteractingElectronsHF numElectrons maxIterations extPotential =
   (totalEnergyHF extPotential bestStates, toSpinOrbitals bestStates)
   where
-    (PhysicalGrid s v) = extPotential
-    densityOf psi = PhysicalGrid s (fmap (waveToProb s) (listArray (bounds v) (elems psi)))
     candidates :: [[WaveState]]
     candidates = map (runSCF maxIterations extPotential) (candidateSpinAssignments numElectrons)
     bestStates :: [WaveState]
@@ -210,16 +263,5 @@ solveInteractingElectronsHF numElectrons maxIterations extPotential =
     scoreOf :: [WaveState] -> Double
     scoreOf states = sum [ e # ElectronVolt | (e, _, _) <- states ]
     toSpinOrbitals :: [WaveState] -> [WaveSolution]
-    toSpinOrbitals states = [ WaveSolution (energy, densityOf psi, Just sp) | (energy, psi, sp) <- states ]
-
--- | The bare one-electron energy <psi|T+V_ext|psi> — kinetic + external potential only
-oneElectronEnergy :: PhysicalGrid Energy -> Vec -> Energy
-oneElectronEnergy extPotential psi = (dotA psi (h0 psi) / dotA psi psi) % ElectronVolt
-  where
-    (h0, _) = hamiltonianVector ElectronVolt extPotential
-
--- | Correct total Hartree-Fock energy: E = 1/2 * sum(h_i + epsilon_i)
-totalEnergyHF :: PhysicalGrid Energy -> [WaveState] -> Energy
-totalEnergyHF extPotential states =
-  (0.5 % Number) |*| foldl1' (|+|)
-    [ h_i |+| epsilon_i | (epsilon_i, psi, _) <- states, let h_i = oneElectronEnergy extPotential psi ]
+    toSpinOrbitals states = [ WaveSolution (energy, mapVecToGrid extPotential psi waveToProb, Just sp) | (energy, psi, sp) <- states ]
+-- ===ENDREGION
--- src/AtomSimulator/Units.hs
+++ src/AtomSimulator/Units.hs
@@ -1,39 +1,42 @@
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
-{-# LANGUAGE FlexibleContexts  #-}
 {-# LANGUAGE TypeOperators #-}
-
+{-# OPTIONS_GHC -Wmissing-signatures -Wmissing-local-signatures #-}
 module AtomSimulator.Units where
-
 import Data.Metrology
 import Data.Metrology.TH
 import Data.Metrology.Show ()
 
+-- ===REGION Base Units
 declareMonoUnit "Nanometer" (Just "nm")
+-- | Length is a physical dimension that quantifies the space between points
+type Length = MkQu_D Nanometer
 declareDerivedUnit "MicroMeter" [t| Nanometer |] 1e3 (Just "um")
 declareDerivedUnit "Meter" [t| Nanometer |] 1e9 (Just "m")
 declareDerivedUnit "Bohr" [t| Nanometer |] 0.0529177249 (Just "m")
 
 declareMonoUnit "ElectronVolt" (Just "eV")
+{-| Energy is a conserved property in physical systems. Particles can have different potential energies at
+different points in space and the energy that a particle in a system have can be quantized.-}
+type Energy = MkQu_D ElectronVolt
 declareDerivedUnit "Joule" [t| ElectronVolt |] 6.241509074e18 (Just "J")
 declareDerivedUnit "MegaElectronVolt" [t| ElectronVolt |] 1e6 (Just "MeV")
 declareDerivedUnit "Hartree" [t| ElectronVolt |] 27.2114 (Just "E h")
 
 declareMonoUnit "AtomicUnit" (Just "Au")
+-- | Mass is a property of particles that decreases the wavelength of particles
+type Mass = MkQu_D AtomicUnit
 declareDerivedUnit "Kilogram" [t| AtomicUnit |] 6.02214076e26 (Just "kg")
 
 declareMonoUnit "ElementaryCharge" (Just "e")
-
-{-| Energy is a conserved property in physical systems. Particles can have different potential energies at
-different points in space and the energy that a particle in a system have can be quantized.-}
-type Energy = MkQu_D ElectronVolt
--- | Length is a physical dimension that quantifies the space between points
-type Length = MkQu_D Nanometer
--- | Mass is a property of particles that decreases the wavelength of particles
-type Mass = MkQu_D AtomicUnit
 -- | Charge is a property of particles that attracts particles with opposite sign charge and repels ones with same sign charge
 type Charge = MkQu_D ElementaryCharge
 
+-- | Spin is an intrinsic form of angular momentum carried by particles. There are 2 orthogonal states of spin for spin 1/2 spin particles
+data Spin = SpinUp | SpinDown deriving (Eq, Show)
+-- ===ENDREGION
+
+-- ===REGION Derived Unit Types
 -- | Density is the amount of something in a given volume of 3D space.
 type Density  = Length %^ MThree
 {-| Speed Squared is an abstract concept that represents the amount of distance covered by an object in a certain
@@ -49,14 +52,12 @@ type InverseLength = Length %^ MOne
 type SpringConstant = MkQu_D (ElectronVolt :/ Nanometer :/ Nanometer)
 -- | A distance times a distances, or an area
 type DistSq = MkQu_D (Nanometer :* Nanometer)
+-- ===ENDREGION
 
--- | A meter per second is a standard SI unit for velocity. This value is that number squared
-meterPerSecondSq :: SpeedSq
-meterPerSecondSq = (1 % Joule) |/| (1 % Kilogram)
-
+-- ===REGION Physical Constants
 -- | Light travels a constant amount of distance in a given amount of time
 lightSpeedSq :: SpeedSq
-lightSpeedSq = (2.99792458*10^8)^2 |*| meterPerSecondSq
+lightSpeedSq = (2.99792458*10^8)^2 |*| (1 % Joule) |/| (1 % Kilogram)
 
 -- | This is the mass of an electron particle at rest. 
 electronMass :: Mass
@@ -67,6 +68,7 @@ protonMass = (938.272 % MegaElectronVolt) |/| lightSpeedSq
 -- | Permittivity of free space
 vacuumPermittivity :: Permittivity
 vacuumPermittivity = (55.26349406 % ElementaryCharge) |*| (1 % ElementaryCharge) |/| (1 % ElectronVolt) |/| (1 % MicroMeter)
+-- ===ENDREGION
 
 -- | The charge of an electron
 electronCharge :: Charge
@@ -74,4 +76,3 @@ electronCharge = (-1) % ElementaryCharge
 -- | The charge of a proton
 protonCharge :: Charge
 protonCharge = 1 % ElementaryCharge
-
